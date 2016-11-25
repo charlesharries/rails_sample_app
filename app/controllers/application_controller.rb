a9d19f0333838e59dd8c5a517f23f0bf6bd3b488
Charles Harries <charlesgharries@gmail.com>
2016-11-24 21:24:15 -0500

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-    render html: "Hey this thing deployed correctly. Good job champ."
-  end
 end

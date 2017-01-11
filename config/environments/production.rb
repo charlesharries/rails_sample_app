6ba5c21e6875079fa39057b2d7b4abcaf07574bf
Charles Harries <charlesgharries@gmail.com>
2017-01-11 13:10:47 -0500

    Fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Configure the mailer to work with SendGrid
-  config.action_mailer.raise_deliver_errors = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = "sortoftwitter.herokuapp.com"
   config.action_mailer.default_url_options = { host: host }
